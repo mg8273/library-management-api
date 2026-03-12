adebd9efc520f4205c9dae3c9536f66638e8dc61
Manu Garg <krypton@Manus-MacBook-Air.local>
2026-03-12 12:08:39 +0530

    fix render port issue

--- server.js
+++ server.js
@@ -12,18 +12,16 @@ app.use(express.json());
 
 app.use("/books", bookRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
-.then(()=> console.log("MongoDB Connected"))
-.catch(err => console.log(err));
-
-const PORT = 3000;
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-
-
 app.get("/", (req,res)=>{
 res.send("API working");
 });
 
+mongoose.connect(process.env.MONGO_URI)
+.then(()=> console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
